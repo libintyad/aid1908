8e5c51167109311fc35964e485b2806e9511d908
Tedu <lb_pyl@163.com>
2019-08-14 11:06:35 +0600

    init commit

---
+++ exercise05.py
@@ -0,0 +1,21 @@
+
+list01=[]
+while True:
+    str01=input("输入字符串：")
+    if str01=="":
+        break
+    list01.append(str01)
+str_result="".join(list01)
+print(str_result)
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ exercise06.py
@@ -0,0 +1,13 @@
+
+#
+# str01="张无忌_张翠山_张三丰"
+# sulit=str01.split("_")
+# print(sulit)
+#
+
+
+message = "How are you"
+list_temp = message.split(" ")
+print(list_temp)
+message = " ".join(list_temp)
+print(message)
